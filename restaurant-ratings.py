2f4dc0723476768b8ea53072f9efb760863d5811
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 18:12:59 -0700

    We added raw input of user's favorite restaurant and rating.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -12,6 +12,19 @@ for restaurant in sorted(restaurant_ratings):
     ratings = restaurant_ratings[restaurant]
     print "Restaurant %s is rated at %s" % (restaurant, ratings)
 
+for restaurant in sorted(restaurant_ratings):
+    ratings = restaurant_ratings[restaurant]
+    print "Please tell us what your favorite restaurant is."
+    restaurant = raw_input()
+    print "What rating would you give to this restaurant?"
+    ratings = int(raw_input())
+    restaurant_ratings[restaurant] = ratings 
+
+    print restaurant 
+    print ratings 
+    print restaurant_ratings
+
+    
+  
 
 
- 
